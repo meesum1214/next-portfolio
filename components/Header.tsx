d79060399977d55195b78a1fadffe4dc34d14a5d
meesum1214 <salmannaqvi461@gmail.com>
2022-12-09 16:51:47 +0500

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+
+type Props = {}
+
+export default function Header({ }: Props) {
+    return (
+        <header>
+            <div className='flex'>
+                {/* Social Icons */}
+                <SocialIcon
+                    url="https://www.linkedin.com/"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+                <SocialIcon
+                    url="https://www.instagram.com/"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+                <SocialIcon
+                    url="https://www.twitter.com/"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+            </div>
+
+            <div>
+                <SocialIcon
+                    className='cursor-pointer'
+                    network="email"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in Touch</p>
+            </div>
+        </header>
+    )
+}
